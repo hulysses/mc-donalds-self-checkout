f69e8c5fc447c1e86501f55986efdadb86f47a05
hulysses <hulyssesmaga@gmail.com>
2025-02-20 15:28:31 -0300

    feat: implement product detail page with dynamic routing and header component

---
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Product } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+interface ProductHeaderProps {
+  product: Pick<Product, "imageUrl" | "name">;
+}
+
+const ProductHeader = ({ product }: ProductHeaderProps) => {
+  const router = useRouter();
+
+  return (
+    <div className="relative w-full h-[300px]">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute top-4 left-4 z-50 rounded-full"
+        onClick={() => router.back()}
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain"
+      />
+
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute top-4 right-4 z-50 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default ProductHeader;
--- src/app/products/page.tsx
+++
@@ -1,8 +0,0 @@
-const ProductsPage = () => {
-  return (
-    <div>
-    </div>
-  );
-};
-
-export default ProductsPage;
